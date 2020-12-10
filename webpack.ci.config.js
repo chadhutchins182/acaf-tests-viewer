546c70a681732fcdacf5dbf0ef402dee4789228c
Chad Hutchins <chadhutchins182@users.noreply.github.com>
2020-12-10 14:29:10 -0800

    Separate ci build

---
+++ webpack.ci.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+
+console.log("TEST ENV: " + JSON.stringify(process.env));
+
+module.exports = merge(common, {
+    mode: 'production',
+    entry: './src/acaf.js',
+    output: {
+        filename: 'acaf.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+});
+
