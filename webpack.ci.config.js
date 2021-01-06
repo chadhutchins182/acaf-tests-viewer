ad9c3a9d4c1b01bf606add5a05c0edd1aab07f77
Chad Hutchins <chadhutchins182@users.noreply.github.com>
2021-01-05 19:08:04 -0800

    Removed bundle-analyzer

--- webpack.ci.config.js
+++ webpack.ci.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
